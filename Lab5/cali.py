a69aa5afe9df93f7ad8386964e39e04218b22f10
bee0511 <bee000092@gmail.com>
2023-11-08 13:51:53 +0800

    Modify cali.py

--- Lab5/cali.py
+++ Lab5/cali.py
@@ -20,35 +20,41 @@ class Calibration:
         object_point[:, :2] = np.mgrid[0:9, 0:6].T.reshape(-1, 2)
         key = -1
 
-        while len(self.object_points) < 12:
+        while len(self.object_points) < 15:
             frame = frame_read.frame
-            cv2.imshow('frame', frame)
+            cv2.imshow("frame", frame)
             grayed_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-            retCorner, corners = cv2.findChessboardCorners(grayed_frame, (self.NX, self.NY), None)
+            retCorner, corners = cv2.findChessboardCorners(
+                grayed_frame, (self.NX, self.NY), None
+            )
             if retCorner and key != -1:
-                corner2 = cv2.cornerSubPix(grayed_frame, corners, self.WIN_SIZE, self.ZERO_ZONE, self.CRITERIA)
+                corner2 = cv2.cornerSubPix(
+                    grayed_frame, corners, self.WIN_SIZE, self.ZERO_ZONE, self.CRITERIA
+                )
+                cv2.drawChessboardCorners(
+                    grayed_frame, (self.NX, self.NY), corners, True
+                )
+                cv2.imshow("grayed_frame", grayed_frame)
+                ret = cv2.waitKey(5000)
+                if ret == -1:
+                    continue
                 self.object_points.append(object_point.copy())
                 self.img_points.append(corner2)
-                cv2.drawChessboardCorners(grayed_frame, (self.NX, self.NY), corners, True)
-                cv2.imshow('grayed_frame', grayed_frame)
-                print('[*] Captured Object', len(self.object_points))
+                print("[*] Captured Object", len(self.object_points))
                 # cv2.imwrite(f'./saved_{str(self.object_points)}.png', grayed_frame)
             key = cv2.waitKey(50)
 
         ret, camera_matrix, distortion_coeffs, rvecs, tvecs = cv2.calibrateCamera(
-            self.object_points,
-            self.img_points, 
-            (self.NX, self.NY), 
-            None, 
-            None
+            self.object_points, self.img_points, (self.NX, self.NY), None, None
         )
 
         self.distortion_coeffs = distortion_coeffs
         self.camera_matrix = camera_matrix
 
         return (camera_matrix, distortion_coeffs)
-    
-if __name__ == '__main__':
+
+
+if __name__ == "__main__":
     drone = Tello()
     drone.connect()
     drone.streamon()
@@ -60,5 +66,5 @@ if __name__ == '__main__':
     f.write("intrinsic", cameraMatrix)
     f.write("distortion", distCoeffs)
     f.release()
-    
+
     drone.streamoff()
